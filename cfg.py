d48a7403dadb868dab21d5eb9f173bdcfe25fb3e
SamFelsted <61361381+SamFelsted@users.noreply.github.com>
2020-10-12 07:05:48 -0700

    commit

---
+++ cfg.py
@@ -0,0 +1,52 @@
+import key
+#Settings
+connection_data = ('irc.chat.twitch.tv', 6667)
+NICK = "SnackBOT"
+
+#Put oauth token here SUPER SECRE DO NOT SHARE \/ \/ \/
+PASS = key.hiddenKey
+
+
+#oauth goes above /\ /\ /\
+
+#Channel Chat ALL LOWER CASE \/ \/ \/
+CHAN = "alphazulu22"
+#Channel ALL LOWER /\ /\ /\
+
+RATE = (20 / 30)
+
+### HERE DOWN FROM EVERY TIME: \/ \/ \/ COPY AND PASTE FROM HERE DOWN \/ \/ \/
+
+questions =  ["Why did you start streaming? Who inspired you to do so?", "What was the first game you ever played? Where and when did you play it?", "What is your favorite game of all time? Why is it great?",
+"What are some games that you enjoy playing alone?", "What are some good games to play with other people?", "Do you prefer to use a mouse + keyboard or a controller?",
+"What odd skills have you learned from playing video games?", "You played video games as a little kid. How old do you think a child should be before he or she is allowed to play video games?", 
+"What is your favorite arcade game?", "Is there an upcoming game you are looking forward to playing? What is it? What do you expect from it?",
+"These days, some gamers make a living from playing video games. Do you think professional gamers are similar to the athletes in other sports? Why or why not?", "Are video games addictive? Why can't some people stop playing?",
+"Some video games are very violent. Do you think they influence their players to be violent in real life? If so, what should we do about it?", "Virtual reality systems are gaining popularity these days. Are you excited about the potential of virtual reality? Or are you worried? Why?",
+"Have you ever played a game that requires physical movement, like with the Nintendo Wii? Do you like this kind of game? What can be some pros to playing games like that?",
+"It seems that most video game players are male. Why do boys like video games more than girls do?", "Nowadays, many 'gamers' play games on their phones or tablets. How do you feel about mobile games and the future of mobile gaming?",
+"Do you get excited by new technology? What kinds of technology interest you?", "Is technology developing faster than it used to? Are you worried about some tech being forgotten?", "What are some of your favorite flash games?", "What are some of your favorite websites?", "Which electronics brands do you trust most? Are you loyal to one brand?",
+"Are you an early adopter? Do you like to have cutting edge technology? Do you feel there are any disadvantages to buying the latest product?", "Some people dislike or fear technology; what are your thoughts on the matter?", "Do you read e-books? What are their advantages and disadvantages when compared with paper books in your opinion?",
+"Is there an electronic product you want these days? What is it? Would it make your life better/easier?", "What crazy future technology are you looking forward to? For example, flying cars or personal robots.", "How do you feel about dating websites or apps? Would you use one?", " Some people prefer to live with less technology in their lives. Can you understand the desire to live a simpler life?", "Many science fiction movies present a dark vision of the future. Are you optimistic or pessimistic about the future of humanity?", "Would you give up video games for life and receive $500,000 in cash, or would you refuse the money?", "In your opinion, what is the worst game of all time that you've played?", "Would you rather have loads of time to play games but only be able to buy two games a year, or be able to buy loads of games but never have much time to play them?", "If you could marry any game character, who would it be?", "If you could have any video game character as your best friend, who would it be?", "Is there a game that is considered to be really bad but is your secret guilty pleasure?", "What's more important: action or story?", "What's your favorite game series of all time?", "Are there any video games you're really bad at?", "What is your favorite game from your childhood?", "Who is your favorite villain from a video game and why?", "If you had to choose to own and exclusively between PlayStation 5 or Xbox Series X, what would you choose and why? (Nintendo not included.)", 
+"Who do you usually choose as your fighter in Super Smash Brothers?", "What makes a good Super Smash Bros character in your opinion?", "If you could have one power of any video game character, who would it be?", "What's a game you've spent the most amount of money on?", "Which video game character do you most wish you were like?", "Have you ever wanted to live in a world from a game? If so, which one and why?", "What is your favorite song from a game?", "If video games didn't exist anymore, what would you do?", "What is the longest amount of time you have sat playing games non-stop?", "What or who got you into video games in the first place?", "What is the first console you ever owned?", "Do you prefer co-op or player vs player?", "Do you prefer multiplayer games or to go solo?", "Have you ever had to stop playing a game because it was too scary?", "Everybody trashes Fortnite because it is the popular thing to do. What's your brutally honest opinion on Fortnite?", "In your opinion, what is the best video game console?", "Have you ever pretended to be sick to get out of something so you could play a game?", "What game have you spent the most amount of hours on?",
+"What's a game you didn't expect to enjoy but ended up liking?", "What's a really popular game everyone seems to love but you don't like?", "Has a game's storyline ever made you cry?", "What is your favorite gaming memory?", "What do you miss about gaming trends from your childhood?", "What are your favorite things about gaming in this day and age?", "What is your favorite Pokemon?", "What is your favorite thing about streaming?", "What is your least favorite thing about streaming?", "If you had to do a job as your career that is associated with gaming, but you can't do Streaming/YouTube, what would you do?", "What is your favorite e-sport to watch?", "What game would you love to speedrun?", "What is a game that you love to watch being speedrun?", "If you could make your own game, what would it be like?", "Who is your favorite BIG streamer?", "Who is your favorite small streamer?", "What would you say to someone who is thinking about streaming?", "What is a hidden gem indie game?", "What is your favorite board game?", "What are your thoughts on the Joy-Con drift situation?", "What are your thoughts on the current state of Pokemon games?",
+"What are somethings about streaming that most people don't see or realize?", "Who is this AlphaZulu22 and how did you meet him?(<3 you scott)", "If you could have any power, what would it be?", "What's the best thing you bulit in minecraft? How long did it take?", "What's something you wish more game devs would do?", "How do you feel about getting older?", "What classes did you take in high schol? What did you wish you took?"] 
+
+
+
+messageSend = ["If you're enjoying the stream, why not drop a follow? We are getting super close to reaching 1000 Followers! If you'd like to support even more, why not subscribe? You can sub for free with Prime Gaming (formerly known as Twitch Prime)  https://gaming.amazon.com/ It supports me monetarily and it is no extra cost to Amazon Prime members! backto10SnackPOG backto10SnackLaughCry backto10SnackHype", "Support Backtosnack: https://streamlabs.com/backtosnack/tip - Donation Link, https://discord.gg/NM5zcfH - Discord, https://twitter.com/backtosnack - Twitter, https://www.instagram.com/backtosnack/?hl=en - Instagram,https://www.tiktok.com/@backtosnack?lang=en - Tik Tok", "Check out one of my YouTube channels: https://www.youtube.com/channel/UCuAYktrxnUb0cdag2cdg8fg, Backtosnack - Stream Highlights + Edited Clipshttps://www.youtube.com/channel/UC34W67eyEJGp0I3C6HmoH5Q?, Backtosnack Stream Archive - Vods https://www.youtube.com/channel/UCFXnuYd5sVEYuTEnu59thXQ?, The Backtosnack Show - Podcast https://www.youtube.com/channel/UChOLTLI3g47RctssSyGAKDQ, Backtosnack IRL - Vlogs, Reviews, etc https://www.youtube.com/channel/UCAmoU6626Py2e0ArrY4dG4A, Daily Streaming News - Literally Daily Streaming New videos https://www.youtube.com/channel/UCFRAmJMYrEQ8E48OAgCJ0VA? "]
+
+
+about = "Howdy, I have been taken over by a program made by AlphaZulu22. I was made in python3."
+
+guns = ["n AR15", " will to live", "n M1 garand", " Pokiball",
+        " gravity gun", " Tec-9", " final smash",
+        " banana gun", " lazor gun", " Shotgun",
+        " pure vitamin b12", " something", " well.. it's weird they just dabbed and... ",
+        "n AWP", "n M9 Berrata"]
+
+attacks = [" kicked ", " shot ", " threw a table at ", " dabbed on ", " flex'd on ", " quoted Jake Paul on ", " punched ", 
+           " blew up ", " snipped ", " memed on ", " has used their final smash on ", " used the Knee Of Justice(TM) on ", " shot an arrow at ", 
+           " slapped ", " used a blow gun on ", " put a pitfall under ", " sidekicked ", " used tackle on ", " used hyperbeam on ", " tazed ", " did something to ", " smited ", " struck down ",
+           " got a pentakill by slaying ", " used the remote bomb rune on ", " fury rushed ", " used splash on ", " sidestepped ", " shot ", " shot "," kicked ", " slpapped "]
+oplist = {}
